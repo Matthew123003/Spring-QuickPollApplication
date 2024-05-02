45a45303819daac768fa108e7b7aab3afdfe7fc7
Matt Kramer <matthew123003@gmail.com>
2024-05-02 13:28:44 -0400

    Started poll creation querries

---
+++ src/main/resources/import.sql
@@ -0,0 +1,15 @@
+insert into poll (poll_id, question) values (1, 'What is your favorite color?');
+insert into poll (poll_id, question) values (2, 'What is your favorite movie?');
+insert into poll (poll_id, question) values (3, 'What is your favorite day?');
+insert into poll (poll_id, question) values (4, 'What is your favorite month?');
+insert into poll (poll_id, question) values (5, 'What is your favorite year?');
+insert into poll (poll_id, question) values (6, 'What is your favorite band?');
+insert into poll (poll_id, question) values (7, 'What is your favorite person?');
+insert into poll (poll_id, question) values (8, 'What is your favorite icecream?');
+insert into poll (poll_id, question) values (9, 'What is your favorite food?');
+insert into poll (poll_id, question) values (10, 'What is your favorite drink?');
+insert into poll (poll_id, question) values (11, 'What is your favorite meal of the day?');
+insert into poll (poll_id, question) values (12, 'How tall are you?');
+insert into poll (poll_id, question) values (13, 'How much do you weigh?');
+insert into poll (poll_id, question) values (14, 'What is your hair color?');
+insert into poll (poll_id, question) values (15, 'What is your eye color?');
