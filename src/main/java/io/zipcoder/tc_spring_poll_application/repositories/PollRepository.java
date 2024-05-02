8d0f4ad41f5460620e8450780d7871d8282a9745
Matt Kramer <matthew123003@gmail.com>
2024-05-02 10:25:06 -0400

    Part 3.1 started

--- src/main/java/io/zipcoder/tc_spring_poll_application/repositories/PollRepository.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/repositories/PollRepository.java
@@ -4,6 +4,6 @@ import io.zipcoder.tc_spring_poll_application.domain.Poll;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface PollRepository extends CrudRepository<Poll, Long> {
 }
