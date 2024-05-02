b69bcde5b74de1bcefe26834b6830076f1991d9a
Matt Kramer <matthew123003@gmail.com>
2024-05-02 13:24:13 -0400

    Finished, only need to add mock poll data

--- src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
@@ -4,14 +4,20 @@ import io.zipcoder.tc_spring_poll_application.domain.Poll;
 import io.zipcoder.tc_spring_poll_application.exception.ResourceNotFoundException;
 import io.zipcoder.tc_spring_poll_application.repositories.PollRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.List;
 
 @RestController
 public class PollController {
@@ -26,6 +32,29 @@ public class PollController {
     public ResponseEntity<Iterable<Poll>> getAllPolls() {
         Iterable<Poll> allPolls = pollRepository.findAll();
         return new ResponseEntity<>(allPolls, HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/polls", method= RequestMethod.GET)
+    @ResponseBody
+    public List<Poll> findPaginated(
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size,
+            UriComponentsBuilder uriBuilder,
+            HttpServletResponse response) {
+
+        // Create a Pageable object
+        Pageable pageable = new PageRequest(page, size);
+
+        // Find paginated results
+        Page<Poll> resultPage = (Page<Poll>) pollRepository.findAll((Iterable<Long>) pageable);
+
+        // Check if the requested page is greater than total pages
+        if (page >= resultPage.getTotalPages()) {
+            throw new ResourceNotFoundException();
+        }
+
+        // Return the content of the page
+        return ResponseEntity.ok().body(resultPage.getContent()).getBody();
     }
 
     @RequestMapping(value="/polls", method=RequestMethod.POST)
